6549784350e58aca7ffc42e97bc542b7e6fd19e8
Nathan Houle <nathan@nathanhoule.com>
2015-02-25 01:47:47 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+'use strict';
+
+var arity = require('arity');
+
+/**
+ * Creates a function that is the [composition](https://en.wikipedia.org/wiki/Function_composition)
+ * of a list of functions, where each function is passed the return value of the previous function.
+ *
+ * Compose is right-associative, which means functions are called in right-to-left order. For
+ * example, the operation `h(g(f()))` is represented as `compose(h, g, f)`.
+ *
+ * @name compose
+ * @api public
+ * @category Function
+ * @see {@link pipe}
+ * @param {...Function} funcs The functions to compose into a single function.
+ * @return {Function} Returns a new function which, when called, will invoke each input function in
+ * right-to-left order and return the result of the final function call.
+ * @example
+ * var add = function(a, b) { return a + b; };
+ * var square = function(x) { return x * x; };
+ *
+ * var addThenSquare = compose(square, add);
+ *
+ * addThenSquare(1, 2);
+ * //=> 9
+ */
+var compose = function compose(/* funcs */) {
+  if (!arguments.length) {
+    throw new Error('Expected at least one argument');
+  }
+
+  var funcs = Array.prototype.slice.call(arguments);
+
+  // Type check all inputs
+  for (var i = 0; i < funcs.length; i += 1) {
+    if (typeof funcs[i] !== 'function') {
+      throw new TypeError('Expected a function but received a ' + typeof funcs[i] + ' at argument index ' + i);
+    }
+  }
+
+  // No point in composing/wrapping a single function
+  if (arguments.length === 1) {
+    return arguments[0];
+  }
+
+  var first = funcs.pop();
+  var rest = funcs.reverse();
+
+  return arity(first.length, function() {
+    var result = first.apply(this, arguments);
+
+    for (var i = 0; i < rest.length; i += 1) {
+      result = rest[i].call(this, result);
+    }
+
+    return result;
+  });
+};
+
+/**
+ * Exports.
+ */
+
+module.exports = compose;
---
+++ test/index.js
@@ -0,0 +1,78 @@
+'use strict';
+
+var assert = require('assert');
+var sinon = require('sinon');
+var compose = require('../');
+
+describe('compose', function() {
+  var add, identity;
+
+  beforeEach(function() {
+    add = function(a, b) { return a + b; };
+    identity = function(val) { return val; };
+  });
+
+  it('should be a function', function() {
+    assert.equal(typeof compose, 'function');
+  });
+
+  it('should have an arity of 0', function() {
+    assert.equal(compose.length, 0);
+  });
+
+  it('should return a function with an arity equal to that of the final argument', function() {
+    var a = function(a, b) {};
+    var b = function(a, b, c) {};
+    var fn = compose(a, b);
+
+    assert.equal(fn.length, b.length);
+  });
+
+  it('should return the original function when passed only one argument', function() {
+    assert.equal(compose(add), add);
+  });
+
+  it('should call functions in right-associative order', function() {
+    var first = sinon.spy();
+    var second = sinon.spy();
+    var third = sinon.spy();
+
+    compose(third, second, first)();
+
+    assert(first.calledBefore(second));
+    assert(second.calledBefore(third));
+  });
+
+  it('should only call each function once', function() {
+    var first = sinon.spy();
+    var second = sinon.spy();
+    var third = sinon.spy();
+
+    compose(third, second, first)();
+
+    assert(first.calledOnce);
+    assert(second.calledOnce);
+    assert(third.calledOnce);
+  });
+
+  it('should pass the results of the previous function call to the next', function() {
+    var increment = function(a) { return add(a, 1); };
+    var first = sinon.spy(increment);
+    var second = sinon.spy(increment);
+    var third = sinon.spy(increment);
+
+    compose(third, second, first)(0);
+
+    assert(first.calledWithExactly(0));
+    assert(second.calledWithExactly(1));
+    assert(third.calledWithExactly(2));
+
+  });
+
+  it('should throw an error if any of its arguments are non-functions', function() {
+    assert.throws(function() { compose('abc'); });
+    assert.throws(function() { compose(true, identity, identity); });
+    assert.throws(function() { compose(identity, 1, identity); });
+    assert.throws(function() { compose(identity, identity, 'fdsa'); });
+  });
+});
